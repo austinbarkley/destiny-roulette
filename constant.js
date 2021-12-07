24cddf3aa990e954f43b96f78c0e61112c71d9ec
Austin <Barkley.a83@gmail.com>
2021-12-06 23:13:32 -0700

    Initialize project using Create React App

---
+++ constant.js
@@ -0,0 +1,65 @@
+// primary
+[Sweet Business
+Sturm
+Vigilance Wing
+Rat King
+Mida Multi Tool
+Crimson
+Jade rabbit
+Huckleberry
+Suros Regime
+Cerberus +1
+Wish Ender
+Malfeasance
+Ace of Spades
+Chaperone
+Izanagi
+Last Word
+Arbalest
+Thorn
+Outbreak Perfected
+Bad Juju
+Lumina
+Monte carlo
+Bastion
+Traveler's Chosen
+Witherhoard
+No Time to Explain
+Hawkmoon
+Dead Man's Tale
+Cryosthesia
+Ager's Scepter]
+
+// secondary
+Cold Heart
+Fighting Lion
+Sunshot
+Graviton Lance
+Skyburner's Oath
+RiskRunner
+Hard Light
+Merciless
+Borealis
+Prometheus Lens
+Polaris Lance
+Telesto
+Trinity Ghoul
+WaveSplitter
+Lord of Wolves
+Jotunn
+Le Monarque
+Tarrabah
+Eriana's Vow
+Divinity
+Symmetry
+Devil's Ruin
+CloudStrike
+Tommy's Matchbook
+Fourth horseman
+Duality
+Ruinous Efigy
+Ticuu's Divination
+Vex Mythoclast
+Lorentz Driver
+
+// heavy
